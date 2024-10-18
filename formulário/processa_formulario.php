0bf782db512b27012f64dca9694dfca5db2c70dc
alinne <134729467+alinnne@users.noreply.github.com>
2024-10-18 10:22:16 -0300

    Add files via upload

---
+++ formulário/processa_formulario.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+
+  
+    if (empty($nome) || empty($email)) {
+        echo "Todos os campos são obrigatórios!";
+    } else {
+        
+        if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            echo "Formato de email inválido!";
+        } else {
+           
+            echo "Nome: " . htmlspecialchars($nome) . "<br>";
+            echo "Email: " . htmlspecialchars($email);
+
+           
+        }
+    }
+}
+?>
